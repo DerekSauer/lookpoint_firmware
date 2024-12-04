278025757b75ac02b78858237d42478dae573954
Derek Sauer <dereksauer.ca@gmail.com>
2024-12-04 13:42:31 -0500

    feat (core): Added Embassy and simple example.
    Signed-off-by: Derek Sauer <dereksauer.ca@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,34 @@
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
+#![cfg_attr(feature = "nightly", feature(impl_trait_in_assoc_type))]
 #![no_main]
 #![no_std]
 
-// Install a panic handler outputting to probe-rs.
+use defmt_rtt as _;
+use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_time::{Duration, Timer};
 use panic_probe as _;
 
-// Install `defmt` as the global logger.
-use defmt_rtt as _;
-
-// Running on a Nordic Semiconductor nRF52840.
-use nrf52840_hal as _;
-
-#[cortex_m_rt::entry]
-fn main() -> ! {
+#[embassy_executor::task]
+async fn blink_led(mut led: Output<'static>, interval: Duration) {
     loop {
-        defmt::info!("Tick!");
-        cortex_m::asm::delay(3_276_800);
+        defmt::info!("LED on!");
+        led.set_high();
+        Timer::after(interval).await;
+
+        defmt::info!("LED off!");
+        led.set_low();
+        Timer::after(interval).await;
     }
+}
+
+#[embassy_executor::main]
+async fn main(task_spawner: embassy_executor::Spawner) {
+    let peripherals = embassy_nrf::init(Default::default());
+
+    let led = Output::new(peripherals.P0_13, Level::Low, OutputDrive::Standard);
+    task_spawner
+        .spawn(blink_led(led, Duration::from_millis(1000)))
+        .expect("The MCU is on fire.");
 }
